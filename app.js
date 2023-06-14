0f2cfa2f17aacf0fb91043b4b571fdb179f6dcf1
egryt <131427672+egryt@users.noreply.github.com>
2023-06-14 21:11:16 +0000

    second

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const mongodb = require('mongodb')
+const Item = require('./models/items')
 const app = express();
+// const MONGO_URI = 'mongodb+srv://sahil:<<<pass>>>@cluster0.qvrhn.mongodb.net/'
+mongodb.MongoClient.connect(MONGO_URI, {})
+.then(client => {
+    console.log("--connection established--", client.db("item-database").databaseName)
+    db = client.db("item-database")
+}).then(collections => {
+    console.log("\n--existing collections--", collections)
+//    return db.collection("Anime").insertMany(newSet)
+    // return db.collection("Items").insertOne({name: "something", task:"jhadu"})
+ 
+})
+
+// mongoose.connect(mongodb).then(() => console.log('connected')).catch(err => console.log(err))
 app.set('view engine', 'ejs');
 app.listen(3000);
 
+
+// app.get('/create-item', (req,res)=> {
+//     const item = new Item({
+//         name:'something',
+//         task:'doable2'
+//     });
+//     item.save().then(result => res.send(result)).catch(err =>
+//         cconsole.log(err))
+
+// })
 app.get('/', (req,res) => {
-    const items = [
-        {name: 'abstain', task: 'build todo'},
-        {name: 'anna', task: 'add items'},
-        {name: 'actor', task: 'build frontend'},
-        {name: 'athra', task: 'add authentication'}
-    ]
-    res.render('index', { items });
+    res.redirect('/get-item');
+    // const items = [
+    //     {name: 'abstain', task: 'build todo'},
+    //     {name: 'anna', task: 'add items'},
+    //     {name: 'actor', task: 'build frontend'},
+    //     {name: 'athra', task: 'add authentication'}
+    // ]
+    // res.render('index', { items });
+})
+
+
+app.get('/get-item', (req,res) => {
+    Item.find().then(result => res.send(result))
+    
 })
 
 app.get('/add-item', (req,res) => {
---
+++ models/items.js
@@ -0,0 +1,16 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    task:{
+        type: String,
+        required: true
+    }
+},{timestamps:true});
+
+const Item = mongoose.model('Item', itemSchema)
+module.exports = Item;
